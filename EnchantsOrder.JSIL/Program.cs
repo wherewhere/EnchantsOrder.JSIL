5c415f459afedbcdcda89cbf7f0d3c55f7d365d1
where where <qq2518766683@outlook.com>
2026-02-28 19:58:21 +0800

    添加项目

---
+++ EnchantsOrder.JSIL/Program.cs
@@ -0,0 +1,52 @@
+﻿using EnchantsOrder;
+using EnchantsOrder.Models;
+using JSIL;
+using JSIL.Meta;
+using System.Reflection;
+
+namespace EnchantsOrder.JSIL
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            dynamic WinJS = Builtins.Global["WinJS"];
+            WinJS.UI.processAll();
+            WinJS.Application.start();
+
+            dynamic jquery = Builtins.Global["$"];
+            jquery((Action)OnLoaded);
+        }
+
+        private static void OnLoaded()
+        {
+            dynamic jquery = Builtins.Global["$"];
+            dynamic selector = jquery("#enchantment-name")[0].winControl;
+            selector.addEventListener("suggestionsrequested", (Action<dynamic>)(eventInfo =>
+            {
+                dynamic collection = eventInfo.detail.searchSuggestionCollection;
+                collection.appendQuerySuggestions(new[] { "test", "test1", "test2" });
+            }));
+
+            dynamic penalty = jquery("#object-penalty");
+            dynamic level = jquery("#enchantment-level");
+            dynamic weight = jquery("#enchantment-weight");
+
+            List<Enchantment> enchantments = [];
+            jquery("#enchantment-add").click((Action)(() =>
+            {
+                string name = selector.queryText;
+                int levelValue = int.Parse(level.val());
+                int weightValue = int.Parse(weight.val());
+                Enchantment enchantment = new Enchantment(name, levelValue, weightValue);
+                enchantments.Add(enchantment);
+            }));
+
+            jquery("#enchantment-start").click((Action)(() =>
+            {
+                int penaltyValue = int.Parse(penalty.val());
+                jquery("#results").text(enchantments.Ordering(penaltyValue));
+            }));
+        }
+    }
+}
---
+++ EnchantsOrder.JSIL/wwwroot/js/index.js
@@ -0,0 +1,86 @@
+﻿"use strict";
+
+System.Int64.prototype[new JSIL.MethodSignature(System.Int32, [System.Int64]).GetNamedKey("CompareTo", true)] = function(value) {
+    if (System.Int64.op_LessThan(this, value)) { return -1; }
+    if (System.Int64.op_GreaterThan(this, value)) { return 1; }
+    return 0;
+};
+
+function runMain() {
+    var asm = JSIL.GetAssembly("EnchantsOrder.JSIL", true);
+
+    Object.defineProperty(System.Linq.Enumerable, new JSIL.MethodSignature(System.Int32, [System.Collections.Generic.IEnumerable$b1.Of(System.Int32)]).GetNamedKey("Min", true), {
+        get: function() {
+            return function(source) {
+                var value;
+                var e = JSIL.Memoize(System.Collections.Generic.IEnumerable$b1.Of(System.Int32).GetEnumerator)().Call(source, null);
+                try {
+                    if (!e.MoveNext()) {
+                        throw new System.Exception("NoElementsException");
+                    }
+                    value = e.get_Current();
+                    while (e.MoveNext()) {
+                        var x = e.get_Current();
+                        if (x < value) {
+                            value = x;
+                        }
+                    }
+                }
+                finally {
+                    if (e !== null) {
+                        e.Dispose();
+                    }
+                }
+                return value;
+            };
+        }
+    });
+
+    Object.defineProperty(System.Linq.Enumerable, new JSIL.MethodSignature(System.Int64, [System.Collections.Generic.IEnumerable$b1.Of(System.Int64)]).GetNamedKey("Max", true), {
+        get: function() {
+            return function(source) {
+                var value;
+                var e = JSIL.Memoize(System.Collections.Generic.IEnumerable$b1.Of(System.Int64).GetEnumerator)().Call(source, null);
+                try {
+                    if (!e.MoveNext()) {
+                        throw new System.Exception("NoElementsException");
+                    }
+                    value = e.get_Current();
+                    while (e.MoveNext()) {
+                        var x = e.get_Current();
+                        if (System.Int64.op_GreaterThan(x, value)) {
+                            value = x;
+                        }
+                    }
+                }
+                finally {
+                    if (e !== null) {
+                        e.Dispose();
+                    }
+                }
+                return value;
+            };
+        }
+    });
+
+    var appends = [
+        new JSIL.MethodSignature(System.Text.StringBuilder, [System.Char]).GetNamedKey("Append", true),
+        new JSIL.MethodSignature(System.Text.StringBuilder, [System.Int32]).GetNamedKey("Append", true),
+        new JSIL.MethodSignature(System.Text.StringBuilder, [System.String]).GetNamedKey("Append", true),
+        new JSIL.MethodSignature(System.Text.StringBuilder, [System.Object]).GetNamedKey("Append", true),
+    ];
+    var originalAppends = {};
+    for (var i = 0; i < appends.length; i++) {
+        var name = appends[i];
+        originalAppends[name] = System.Text.StringBuilder.prototype[name];
+        Object.defineProperty(System.Text.StringBuilder.prototype, name, {
+            get: function() {
+                return function(value) {
+                    return originalAppends[name].call(this, value) || this;
+                };
+            }
+        });
+    }
+
+    JSIL.InvokeEntryPoint(asm, []);
+};
